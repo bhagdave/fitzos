10eeaa30a625bd3470462f7e9d6d808e247c9f95
bhagdave <dave_gill@blueyonder.co.uk>
2013-10-14 22:20:35 +0100

    Fixed problem with the Athlete Profile page
    Fixed problem with the Athlete Profile page not going back to the
    athlete welcome page.

--- fuel/application/controllers/athlete.php
+++ fuel/application/controllers/athlete.php
@@ -70,28 +70,33 @@ class Athlete extends CI_Controller{
 	function profile(){
 		$this->load->model('athletes_model','athletes');
 		if (isset($_POST['age'])){	
+			$this->load->model('members_model','members');
 			// post to the database baby...
 			$data = $_POST;
 			$data['id'] = $this->session->userdata('id');
 			$this->athletes->saveProfile($data);
 			$this->session->set_flashdata('message', 'Profile Saved');			
 			$athlete = $this->athletes->loadProfile($data['id']);
+			// get the athlete from the database
+			$member  = $this->members->getMember($data['id']);
+			$vars = array('athlete'=>$athlete,'member'=>$member);
+			$this->fuel->pages->render('athlete/welcome',$vars);
 		} else {
 			if ($this->session->userdata('id')){
 				// get the athlete from the database
 				$id      = $this->session->userdata('id');
 				$athlete = $this->athletes->loadProfile($id);
+				if (isset($athlete)){
+					$vars = array('athlete'=>$athlete);
+					$vars['message'] = $this->session->flashdata('message');
+					$this->fuel->pages->render('athlete/profile',$vars);	
+				} else {
+					redirect('signin/login');
+				}			
 			} else {
 				redirect('signin/login');
 			}
 		}
-		if (isset($athlete)){
-			$vars = array('athlete'=>$athlete);
-			$vars['message'] = $this->session->flashdata('message');
-			$this->fuel->pages->render('athlete/profile',$vars);	
-		} else {
-			redirect('signin/login');
-		}			
 	}
 }
 ?>
