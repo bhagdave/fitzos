8f3eb6670d2230221c7236df0ccf80d401463c0b
bhagdave <dave_gill@blueyonder.co.uk>
2013-10-19 12:39:49 +0100

    Show dropdown of sports in a form.

--- fuel/application/views/athlete/sports.php
+++ fuel/application/views/athlete/sports.php
@@ -1,5 +1,4 @@
 <?php 
-var_dump($athlete);
 var_dump($sports);
 ?>
 <div class="row">
@@ -8,4 +7,24 @@ var_dump($sports);
 	</div>
 </div>
 <div class="row-fluid">
+	<div class="span4">
+		<h2>Your Existing Sports</h2>
+	</div>
+	<div class="span4">
+	</div>
+	<div class="span4">
+		<h2>Add a Sport</h2>
+		<form action="" method="post">
+			<input type="hidden" name="member_id" value="<?=$athlete->member_id ?>" />
+			<fieldset>
+				<select name="sport_id">
+					<?php
+						foreach($sports as $sport){
+							echo("<option value='" . $sport['id'] . "'>".$sport['name']."</option>");
+						} 
+					?>
+				</select>
+			</fieldset>
+		</form>
+	</div>
 </div>
