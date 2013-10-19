786b8dc71e1b69c8f9046cef0c501b1e6b982cc4
root <root@ip-10-234-35-105.eu-west-1.compute.internal>
2013-10-19 10:49:38 +0000

    Fixed database config for live site

--- fuel/application/cache/dwoo/compiled/33f3fc440303c2b5de7e73f2c1fd529b.d17.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ?><!--__FUEL_MARKER__0-->Please complete as much of the profile as possible as this will enable us to match you to the correct groups/trainers etc.<?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
--- fuel/application/cache/dwoo/compiled/bd21bb21d4b8de45302307fa62020477.d17.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ?><!--__FUEL_MARKER__0-->Blah, Blah..
-
-Please fill in the shit on the left hand side... Because, well because it helps us to fit you up with shit you might be interested in, not stuff to sell you.. Well okay it might be stuff to sell you but we were thinking more like sporting events that you could take part in.  But if you do not fill in the shit over there how are we going to know what you are interested in...<?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
