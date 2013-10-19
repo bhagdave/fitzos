42ad6f57b5b7ed00a07306c5078ab180f5014ae5
bhagdave <dave_gill@blueyonder.co.uk>
2013-10-19 16:40:37 +0100

    Attempted fix to the header and order of JS

--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -4,13 +4,12 @@
 <title>FitZos.com</title>
 <meta name="fitness socialized" content="FitZos.com">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../assets/css/style.css">
 <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
 <link href="../assets/css/bootstrap.css" rel="stylesheet">
-<script src="../assets/js/jquery.js"></script>
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <link href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
+<link rel="stylesheet" href="../assets/css/style.css">
 
 <!--<link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>-->
 </head>
