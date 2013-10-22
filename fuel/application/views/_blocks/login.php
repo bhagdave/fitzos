8efc597b7f6f58df0b9a7846c324fa96bdf0888b
bhagdave <dave_gill@blueyonder.co.uk>
2013-10-22 21:52:57 +0100

    Changed the form action so local versions work.

--- fuel/application/views/_blocks/login.php
+++ fuel/application/views/_blocks/login.php
@@ -1,5 +1,5 @@
 	<section>
-		<form action="http://beta.fitzos.com/signin/login" method="post">
+		<form action="/signin/login" method="post">
 		<h5>Log In</h5>
 		<fieldset class="loginform" id="inputs">
 			<input name="username" id="username" type="text" placeholder="Username" autofocus required>   
@@ -7,7 +7,7 @@
 		</fieldset>
 		<div class="btn-group">
 			<button class="btn-small btn-primary">Log in</button>
-			<a href="http://beta.fitzos.com/signin/forgotPassword" class="btn-small">Forgot your password?</a>
+			<a href="/signin/forgotPassword" class="btn-small">Forgot your password?</a>
 		</div>
 		</form>
 	</section>
