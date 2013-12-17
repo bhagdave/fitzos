11af9886b70fe06ac611650188e2d1cd8f4fd3f5
jx-fitzos <orione96@Jims-iMac.local>
2013-12-16 21:02:19 -0500

    home.php

--- fuel/application/views/home.php
+++ fuel/application/views/home.php
@@ -13,12 +13,21 @@
 <div class="row-fluid">
 	<div class="span12">
 		<div class="row-fluid">
+			<div class=“span12 splash”>
+				<img src="../../../assets/images/splash_lead1.jpg" alt=""/>
+			</div>
+			
 			<div class="span4 signin">
 				<?php $this->load->view('_blocks/sign-in');?>
 			</div>
+<<<<<<< HEAD
+			<!--
 			<div class="span4 login">
 				<?php $this->load->view('_blocks/login');?>
 			</div>
+			-—>
+=======
+>>>>>>> 23dd39a853516d6c5302ed4503b586a2b437f76a
 		</div>
 	</div>
 </div>
