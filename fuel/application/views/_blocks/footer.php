f88056fcdc3b46fcd2c48d92b810a11cac91d31c
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-13 20:44:20 +0000

    AddThis Added to footer

--- fuel/application/views/_blocks/footer.php
+++ fuel/application/views/_blocks/footer.php
@@ -3,5 +3,26 @@
 	</div>
 </div>
 <?php echo js('main').js($js); ?>
+<!-- AddThis Smart Layers BEGIN -->
+<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-52d44f4a5cedc019"></script>
+<script type="text/javascript">
+  addthis.layers({
+    'theme' : 'transparent',
+    'share' : {
+      'position' : 'right',
+      'numPreferredServices' : 5
+    }, 
+    'follow' : {
+      'services' : [
+        {'service': 'facebook', 'id': 'meetotherathletes'},
+        {'service': 'twitter', 'id': 'whatsyourpeak'}
+      ]
+    },  
+    'whatsnext' : {},  
+    'recommended' : {} 
+  });
+</script>
+<!-- AddThis Smart Layers END -->
 </body>
 </html>
