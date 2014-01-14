86462c9199cd431cd531c44465a119d5e5232623
bhagdave <dave_gill@blueyonder.co.uk>
2014-01-14 11:19:12 +0000

    Added uploaded image to Athlete view page

--- fuel/application/views/athlete/view.php
+++ fuel/application/views/athlete/view.php
@@ -3,6 +3,13 @@
 		<h2><?=isset($member) ? $member->first_name . ' ' . $member->last_name : '' ; ?></h2>
 	</div>
 	<div class="span4">
+		<?php 
+			if (isset($member) && isset($member->image)){
+				echo("<img src='/$member->image'>");
+			}
+		?>
+	</div>
+	<div class="span4">
 		<h2>Their Sports</h2>
 	<?php
 		if (isset($sports)){
