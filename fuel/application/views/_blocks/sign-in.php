6bb4265597ab28318421989c60d9e08b91dadcec
bhagdave <dave_gill@blueyonder.co.uk>
2014-01-17 15:16:06 +0000

    More Knockout testing stuff

--- fuel/application/views/_blocks/sign-in.php
+++ fuel/application/views/_blocks/sign-in.php
@@ -10,16 +10,44 @@
   			Trainer
 		</label>
 		<fieldset id="inputs">
-			<input id="name" name="name" value="<?=isset($request['name']) ? $request['name'] : ''  ?>" type="text" placeholder="Name" autofocus required>   
-			<input id="email" name="email" value="<?=isset($request['email']) ? $request['email'] : ''  ?>" type="text" placeholder="Email" required>
-			<input id="password" name="password" value="<?=isset($request['password']) ? $request['password'] : ''  ?>" type="password" placeholder="Password" required>
-			<input id="confirm_password" name="confirm_password" value="<?=isset($request['confirm_password']) ? $request['confirm_password'] : ''  ?>" type="password" placeholder="Confirm Password" required>			
+			<input id="name" data-bind="value: name" name="name" value="<?=isset($request['name']) ? $request['name'] : ''  ?>" type="text" placeholder="Name" autofocus required>   
+			<input id="email" data-bind="value: email" name="email" value="<?=isset($request['email']) ? $request['email'] : ''  ?>" type="text" placeholder="Email" required>
+			<input id="password" data-bind="value: password,valueUpdate: 'afterkeydown'" name="password" value="<?=isset($request['password']) ? $request['password'] : ''  ?>" type="password" placeholder="Password" required>
+			<input id="confirm_password" data-bind="value: confirm,valueUpdate: 'afterkeydown'" name="confirm_password" value="<?=isset($request['confirm_password']) ? $request['confirm_password'] : ''  ?>" type="password" placeholder="Confirm Password" required>			
 			<br>
+			<p><span data-bind="visible: matched" >Your passwords do not match</span></p>
+			
 			<label class="radio inline">
   				<input type="radio" name="TC" id="TC" value="Accept">
   				<small>I accept these <br clear> <a href="terms">terms and conditions</a></small>
 			</label>
-			<button class="btn btn-success btn-mini">Sign me up</button>
+			<button data-bind="twButton">Sign me up</button>
 		</fieldset>
 		</form>
 	</section>
+<script type="text/javascript">
+	function SignupModel(){
+		var self   = this;
+		self.name  = ko.observable();
+		self.email = ko.observable();
+		self.password = ko.observable();
+		self.confirm  = ko.observable();
+		self.matched  = ko.computed(function(){
+			return self.confirm() != self.password();	
+		},this);
+		self.completed = ko.computed(function(){
+			return (name && email && password && confirm) && !self.matched;
+		},this);
+	};
+	ko.bindingHandlers.twButton = {
+		init  : function(element){
+			console.log("AAAAAAAAA");
+			$(element).addClass('btn btn-success btn-mini disabled');
+			$(element).prop('disabled',true);
+		},
+		update: function(element, valueAccessor){}
+	};
+	ko.applyBindings(new SignupModel());
+</script>
+	
+
