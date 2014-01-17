e52855ffa62b894929e5c0c111aaa084f6f40785
bhagdave <dave_gill@blueyonder.co.uk>
2014-01-17 12:40:14 +0000

    Added knockout to login box

--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -2,12 +2,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>ReachYourPeak</title>
-<meta name="fitness socialized" content="FitZos.com">
+<meta name="fitness socialized" content="www.reach-your-peak.com">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link href="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/css/bootstrap-responsive.css" rel="stylesheet">
 <link href="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/css/bootstrap.css" rel="stylesheet">
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/knockout/2.3.0/knockout-min.js"></script> 
 <script src="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/js/jquery.form.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <link href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
--- fuel/application/views/_blocks/login.php
+++ fuel/application/views/_blocks/login.php
@@ -2,12 +2,20 @@
 		<form action="/signin/login" method="post">
 		<h5>Log In</h5>
 		<fieldset class="loginform" id="inputs">
-			<input name="username" id="username" type="text" placeholder="Username" autofocus required>   
-			<input name="password" id="password" type="password" placeholder="Password" required>
+			<input name="username" id="username" data-bind="value:username" type="text" placeholder="Username" autofocus required>   
+			<input name="password" id="password" data-bind="value:password" type="password" placeholder="Password" required>
 		</fieldset>
 		<div class="btn-group">
-			<button class="btn-small btn-primary">Log in</button>
-			<a href="/signin/forgotPassword" class="btn-small">Forgot your password?</a>
+			<button data-bind="enable: username && password" class="btn-small btn-primary" >Log in</button>
+			<a href="/signin/forgotPassword" class="">Forgot your password?</a>
 		</div>
 		</form>
 	</section>
+<script type="text/javascript">
+	function loginModel(){
+		var self = this;
+		self.username = ko.observable();
+		self.password = ko.observable();
+	};
+	ko.applyBindings(new loginModel());
+</script>
