b3347ca1bcc91cb42cd4bfbf1015a3c7c5906b2e
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-17 22:06:29 +0000

    Removed unfinished bits from home page

--- fuel/application/views/home.php
+++ fuel/application/views/home.php
@@ -21,16 +21,12 @@
 	<div class="span12">
 		<div class="row-fluid">
 			<div class="span8 info">
-				<h1>Information and Material</h1>
 			</div>
 			<div class="span4 cal">
-				<p>Link to modified Calendar</p>
 			</div>
 			<div class="span4 photo">
-				<p>link to photo page</p>
 			</div>
 		</div>
 	</div>
 </div>
-<a href="https://plus.google.com/111706559051974640132" rel="publisher">Google+</a>
 
