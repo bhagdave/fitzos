12b6f28881d5c3ca3de97fbb9b06982ba181519d
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-27 16:26:50 +0000

    Fixed problem in notifications model

--- fuel/application/models/notifications_model.php
+++ fuel/application/models/notifications_model.php
@@ -1,5 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
- 
+require_once(FUEL_PATH.'models/base_module_model.php');
+
 class Notifications_model extends Base_module_model {
  
     function __construct()
