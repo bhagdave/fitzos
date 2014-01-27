65d3db5f3ed719e2eb7bd99204141b9749bcf426
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-27 21:50:40 +0000

    Fixed login page issues

--- fuel/application/views/_blocks/login.php
+++ fuel/application/views/_blocks/login.php
@@ -2,20 +2,12 @@
 		<form action="/signin/login" method="post">
 		<h5>Log In</h5>
 		<fieldset class="loginform" id="inputs">
-			<input name="username" class="form-control" id="username" data-bind="value:username" type="text" placeholder="Username" autofocus required>   
-			<input name="password" class="form-control" id="password" data-bind="value:password" type="password" placeholder="Password" required>
+			<input name="username" class="form-control" id="username" type="text" placeholder="Username" autofocus required>   
+			<input name="password" class="form-control" id="password" type="password" placeholder="Password">
 		</fieldset>
 		<div class="btn-group">
-			<button data-bind="enable: username && password" class="btn-small btn-primary" >Log in</button>
+			<button class="btn-small btn-primary" >Log in</button>
 			<a href="/signin/forgotPassword" class="">Forgot your password?</a>
 		</div>
 		</form>
 	</div>
-<script type="text/javascript">
-	function loginModel(){
-		var self = this;
-		self.username = ko.observable();
-		self.password = ko.observable();
-	};
-	ko.applyBindings(new loginModel());
-</script>
