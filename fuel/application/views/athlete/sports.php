1d2ace8b8da9e392bfc7ac5fbe215bb4e16a1673
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-28 13:46:13 +0000

    Tidy up of sports page

--- fuel/application/views/athlete/sports.php
+++ fuel/application/views/athlete/sports.php
@@ -2,54 +2,52 @@
 $existingSports = array();
 ?>
 <div class="row">
-	<div class="col-md-4">
+	<div class="col-md-12">
 		<?= isset($message) ? $message :'' ; ?>
 	</div>
 </div>
-<div class="col-md-6">
-<div class="row">
-	<div class="col-md-4">
-		<h2>Your Existing Sports</h2>
-	<?php
-		if (isset($members_sports)){
-			foreach($members_sports as $sport){
-				echo("<a href='/athlete/stats/{$sport->id}'><h4>$sport->sport</h4></a>");
-				if (isset($sport->from_date)){
-					echo("From $sport->from_date");
+<div class="col-md-12">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-1">
+			<h2>Your Existing Sports</h2>
+		<?php
+			if (isset($members_sports)){
+				foreach($members_sports as $sport){
+					echo("<a href='/athlete/stats/{$sport->id}'><h4>$sport->sport</h4></a>");
+					if (isset($sport->from_date)){
+						echo("From $sport->from_date");
+					}
+					if (isset($sport->to_date)){
+						echo(" Until $sport->to_date");
+					}
+					$existingSports[] = $sport->sport;
 				}
-				if (isset($sport->to_date)){
-					echo(" Until $sport->to_date");
-				}
-				$existingSports[] = $sport->sport;
 			}
-		}
-	?>
+		?>
+		</div>
+		<div class="col-md-4">
+			<h2>Add a Sport</h2>
+			<form action="/athlete/sports" method="post">
+				<input type="hidden" name="member_id" value="<?=$athlete->member_id ?>" />
+				<fieldset>
+				<div class="ui-widget">
+					<select id="sportsbox" name="sport_id">
+	    				<option value="">Select one...</option>
+						<?php
+							foreach($sports as $sport){
+								if (!in_array($sport['name'],$existingSports)){
+									echo("<option value='" . $sport['id'] . "'>".$sport['name']."</option>");
+								}
+							} 
+						?>
+					</select>
+					<input placeholder="From Date" type="text" class="datepicker" name="from_date" />
+					<input placeholder="To date" type="text" class="datepicker" name="to_date" />	
+					<button class="btn btn-success">Submit</button>	
+				</div>
+				</fieldset>
+			</form>
+			<a href="#"  class="btn js-addASport">Add a new sport!</a>
+		</div>
 	</div>
-	<div class="col-md-4">
-	</div>
-	<div class="col-md-4">
-		<h2>Add a Sport</h2>
-		<form action="/athlete/sports" method="post">
-			<input type="hidden" name="member_id" value="<?=$athlete->member_id ?>" />
-			<fieldset>
-			<div class="ui-widget">
-				<select id="sportsbox" name="sport_id">
-    				<option value="">Select one...</option>
-					<?php
-						foreach($sports as $sport){
-							if (!in_array($sport['name'],$existingSports)){
-								echo("<option value='" . $sport['id'] . "'>".$sport['name']."</option>");
-							}
-						} 
-					?>
-				</select>
-				<input placeholder="From Date" type="text" class="datepicker" name="from_date" />
-				<input placeholder="To date" type="text" class="datepicker" name="to_date" />	
-				<button class="btn btn-success">Submit</button>	
-			</div>
-			</fieldset>
-		</form>
-		<a href="#"  class="btn js-addASport">Add a new sport!</a>
-	</div>
-</div>
 </div>
