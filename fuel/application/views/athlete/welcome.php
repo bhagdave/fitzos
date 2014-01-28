9fe8c8eb9250909ab2951b7e510a07715c42ba33
Dave Gill <dave_gill@blueyonder.co.uk>
2014-01-28 11:46:57 +0000

    Tidy up of athlete landing page

--- fuel/application/views/athlete/welcome.php
+++ fuel/application/views/athlete/welcome.php
@@ -1,32 +1,31 @@
 <div class="row">
-	<div class="col-md-4">
+	<div class="col-md-8 col-lg-10 col-xs-12">
 		<h2>Athlete Home Page</h2>
-	</div>
-	<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-4 col-xs-12 col-lg-6">
+					<h2>Notifications</h2>
+				</div>
+				<div class="col-md-4 col-xs-12 col-lg-6">
+					<?php $this->load->view('athlete/notifications'); ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-xs-12 col-lg-6">
+					<h2>Events</h2>
+				</div>
+				<div class="col-md-4 col-xs-12 col-lg-6">
+					<?php $this->load->view('athlete/events'); ?>
+				</div>
+			</div>			
+		</div>
+	<div class="col-md-4 col-lg-2 col-xs-12">
 		<?php 
 			if (isset($member->image)){
-				echo("<img src='/".$member->image."' alt='".$member->first_name . " " . $member->last_name ."' title='".$member->first_name . " " . $member->last_name ."'");
+				echo("<img height=320px src='/".$member->image."' alt='".$member->first_name . " " . $member->last_name ."' title='".$member->first_name . " " . $member->last_name ."'");
 			}
 		?>
-	</div>
-	<div class="col-md-4">
+		<br>
 		<h3><?=isset($member) ? $member->first_name . ' ' . $member->last_name : '' ; ?></h3>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-2 col-xs-12 col-lg-6">
-		<h2>Notifications</h2>
-	</div>
-	<div class="col-md-10 col-xs-12 col-lg-6">
-		<?php $this->load->view('athlete/notifications'); ?>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-2 col-xs-12 col-lg-6">
-		<h2>Events</h2>
-	</div>
-	<div class="col-md-10 col-xs-12 col-lg-6">
-		<?php $this->load->view('athlete/events'); ?>
 	</div>
 </div>
 <div class="row">
@@ -35,40 +34,28 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-1">
-	</div>
-	<div class="col-md-10">
+	<div class="col-md-offset-1 col-md-10">
 		<?php echo fuel_block('athlete_welcome');?>
 	</div>
-	<div class="col-md-1">
-	</div>
 </div>
 <div class="row">
-	<div class="col-md-5"></div>
-	<div class="col-md-2">
+	<div class="col-md-2 col-md-offset-5">
 		<a href="/athlete/profile" width="400px" class="btn btn-success btn-block">Profile</a>
 	</div>
-	<div class="col-md-5"></div>
 </div>
 <div class="row">
-	<div class="col-md-5"></div>
-	<div class="col-md-2">
+	<div class="col-md-2 col-md-offset-5">
 		<a href="/athlete/sports" class="btn btn-success btn-block">Sports</a>
 	</div>
-	<div class="col-md-5"></div>
 </div>
 <div class="row">
-	<div class="col-md-5"></div>
-	<div class="col-md-2">
+	<div class="col-md-2 col-md-offset-5">
 		<a href="/athlete/teams" class="btn btn-success btn-block">Teams</a>
 	</div>
-	<div class="col-md-3"></div>
 </div>
 <div class="row">
-	<div class="col-md-5"></div>
-	<div class="col-md-2">
+	<div class="col-md-2 col-md-offset-5">
 		<a href="/athlete/notifications" class="btn btn-success btn-block">Notifications</a>
 	</div>
-	<div class="col-md-3"></div>
 </div>
 		
