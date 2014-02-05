4057dc0194f219456a55bbcf1b8e08c05da2c750
Dave Gill <dave_gill@blueyonder.co.uk>
2014-02-05 21:58:43 +0000

    Added athlete details to athlete view page

--- fuel/application/views/athlete/view.php
+++ fuel/application/views/athlete/view.php
@@ -1,6 +1,12 @@
 <div class="row">
 	<div class="col-md-4">
 		<h2><?=isset($member) ? $member->first_name . ' ' . $member->last_name : '' ; ?></h2>
+		<p>Nickname:<?= isset($athlete->nickname) ? $athlete->nickname :'' ?></p>
+		<p>DOB:<?= isset($athlete->dob) ? $athlete->dob :'' ?></p>
+		<p>Gender:<?= isset($athlete->gender) ? $athlete->gender :'' ?></p>
+		<p>Activity:<?= isset($athlete->activity) ? $athlete->activity :'' ?></p>
+		<p>Country:<?= isset($athlete->country) ? $athlete->country :'' ?></p>
+		<p>Location:<?= isset($athlete->location) ? $athlete->location :'' ?></p>
 	</div>
 	<div class="col-md-4">
 		<?php 
