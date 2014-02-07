256bfe06610a43f2e56484a537ae21b8062158e3
Dave Gill <dave_gill@blueyonder.co.uk>
2014-02-07 23:23:45 +0000

    Added event link on welcome page

--- fuel/application/views/athlete/events.php
+++ fuel/application/views/athlete/events.php
@@ -2,7 +2,7 @@
 			if (isset($events) && count($events) > 0){
 				foreach($events as $post){
 					echo("<div class='athleteEvents' style='border:1px solid;border-radius:10px;'>");
-					echo("<h4>".$post->name."</h4>");
+					echo("<h4><a href='/event/view/".$post->id."'>".$post->name."</a></h4>");
 					if (!empty($post->content)){
 						echo($post->content. "<br>");
 					}
@@ -11,7 +11,7 @@
 						echo($date->format('m/d/Y'). "<br>");
 					}
 					if (isset($post->image)){
-						echo("<img height=320px src='/".$post->image."' alt='".$post->name."' title='".$post->name."'>");
+						echo("<a href='/event/view/".$post->id."'><img height=320px src='/".$post->image."' alt='".$post->name."' title='".$post->name."'></a>");
 					}
 					echo("</div>");
 				}
