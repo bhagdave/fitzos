62dc05b018f5145bb1e83c307dfa3725e4080fa7
Dave Gill <dave_gill@blueyonder.co.uk>
2014-03-03 15:37:03 +0000

    Fixed link on suggestions.

--- fuel/application/views/find/search.php
+++ fuel/application/views/find/search.php
@@ -4,13 +4,13 @@
 			if (count($suggestions['locations'])>0){
 				echo("<h2>Members with a similar location</h2>");
 				foreach($suggestions['locations'] as $location){
-					echo("<a href='athlete/view/$location->id'>$location->first_name $location->last_name</a>");	
+					echo("<a href='/athlete/view/$location->id'>$location->first_name $location->last_name</a>");	
 				}
 			}
 			if (count($suggestions['sports'])>0){
 				echo("<h2>Members with the same sports</h2>");
 				foreach($suggestions['sports'] as $sport){
-					echo("<a href='athlete/view/$sport->id'>$sport->first_name $sport->last_name</a>");
+					echo("<a href='/athlete/view/$sport->id'>$sport->first_name $sport->last_name</a>");
 				}
 			}
 		}
