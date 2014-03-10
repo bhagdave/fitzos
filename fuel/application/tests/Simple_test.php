3e5a7043cd894ce525169c8dd25501f998408ea9
Dave Gill <dave_gill@blueyonder.co.uk>
2014-03-10 22:12:07 +0000

    Changed SimpleTest to test test framework

--- fuel/application/tests/Simple_test.php
+++ fuel/application/tests/Simple_test.php
@@ -13,8 +13,8 @@ class Simple_test extends Tester_base {
 		$this->load_sql('fitzos_test.sql', NULL);
 	}
 	public function test_myname(){
-		$test = 'Dave';
-		$expected = "Dave";
-		$this->run($test,$expected,"MyName","Does this work!!");
+		$test = 'TEST';
+		$expected = "TEST";
+		$this->run($test,$expected,"SimpleTest","Testing test framwork!!");
 	}
 }
