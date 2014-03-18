c3d2cd715c5cfe4eb4dabf19c67d594ca846cadc
Dave Gill <dave_gill@blueyonder.co.uk>
2014-03-18 18:55:54 +0000

    Display public events on index

--- fuel/application/views/calendar/public.php
+++ fuel/application/views/calendar/public.php
@@ -1,10 +1,22 @@
 <div class="calendar-small">
+	<h2>Public events</h2>
 <?php 
 	if (isset($public)){
-		foreach($public as $event){
-			echo("<div class='event'>");
-			echo($event->id);
+		foreach($public as $post){
+			echo("<div class='athleteEvents' style='border:1px solid;border-radius:10px;'>");
+			echo("<h4><a href='/event/view/".$post->id."'>".$post->name."</a></h4>");
+			if (!empty($post->content)){
+				echo($post->content. "<br>");
+			}
+			if (!empty($post->date)){
+				$date = new DateTime($post->date);
+				echo($date->format('m/d/Y'). "<br>");
+			}
+			if (isset($post->image)){
+				echo("<a href='/event/view/".$post->id."'><img height=320px src='/".$post->image."' alt='".$post->name."' title='".$post->name."'></a>");
+			}
 			echo("</div>");
+			
 		}
 	}
 ?>
