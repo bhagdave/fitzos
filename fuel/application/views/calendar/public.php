2320dd6d4c142e7699ace8aba873557c828302f6
Dave Gill <dave_gill@blueyonder.co.uk>
2014-03-19 14:29:15 +0000

    Amended class on events

--- fuel/application/views/calendar/public.php
+++ fuel/application/views/calendar/public.php
@@ -1,9 +1,9 @@
 <section class="calendar-small">
-	<h2>Public events</h2>
+	<h2>Upcoming events</h2>
 <?php 
 	if (isset($public)){
 		foreach($public as $post){
-			echo("<div class='athleteEvents' style='border:1px solid;border-radius:10px;'>");
+			echo("<div class='event' style='border:1px solid;border-radius:10px;'>");
  			echo("<h4><a href='/event/view/".$post->id."'>".$post->name." ".$post->sport."</a></h4>");
 			if (!empty($post->content)){
 				echo($post->content. "<br>");
