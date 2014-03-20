e98cf3454e15d975301838361f78b522fc393ff8
Dave Gill <dave_gill@blueyonder.co.uk>
2014-03-20 16:35:12 +0000

    Cosmetic changes to make the view better

--- fuel/application/views/calendar/bySport.php
+++ fuel/application/views/calendar/bySport.php
@@ -1,11 +1,14 @@
+<h2>Upcoming events</h2>
 <?php
-if (isset($sportsForThisMonth)){
-	echo("<div class='SportsForThisMonth'");
+if (isset($sportsForThisMonth) && count($sportsForThisMonth)>0){
+	echo("<div class='SportsForThisMonth'>");
 	foreach($sportsForThisMonth as $sport){
 		echo("<div class='sportCount'>");
 		echo("There are $sport->count $sport->name events this month");
 		echo("</div>");	
 	}
-	echo('</div>');
+	echo("</div>");
+} else {
+	echo("<h4>None at the moment. Check again later!</h4>");
 }
 
