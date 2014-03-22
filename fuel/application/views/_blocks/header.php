1e8ef8bf5b54b9194a10162139e91ff53e4a820f
Russ Brown <poa02rb@yahoo.co.uk>
2014-03-22 17:51:41 +0000

    Ok merged conflicts

--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -15,9 +15,10 @@
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/knockout/2.3.0/knockout-min.js"></script> 
+<script src="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/js/jquery.timepicker.min.js"></script>
+<script src="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/js/jquery.form.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <link href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">
-<script src="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/js/production.min.js"></script>
 
 <!--<link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>-->
 </head>
@@ -27,43 +28,12 @@
 			<a href="http://<?=$_SERVER['SERVER_NAME'] ?>">
 				<img src="http://<?=$_SERVER['SERVER_NAME'] ?>/assets/images/logo.png" alt="www.reach-your-peak.com" name="www.reach-your-peak.com" width="89" height="49" id="FitZos" class="logo"/>
 			</a>
-<<<<<<< HEAD
+			<?php
+				if (isset($this->session)){
+					echo($this->session->flashdata('message'));
+				} 
+			?>
 		</header>
 
-=======
-<?php
-	$ci = &get_instance(); 
-	$session = $ci->load->library("session");
-	$userId  = $session->userdata('id');
-	if (isset($userId)){
-		$memberModel  = $ci->load->model("members_model");
-		$member = $memberModel->getMember($userId);
-		if (isset($member)){
-			echo("<div class='welcome'><a href='http://" .$_SERVER['SERVER_NAME']."/athlete/index'>Welcome " . $member->first_name . ' ' . $member->last_name.'</a></div>');
-			echo("<a href='http://" .$_SERVER['SERVER_NAME']."/signin/logout'>Logout</a></div>'");
-		} else {
-?>
-		<div class="signin">
-			<a href="http://<?=$_SERVER['SERVER_NAME'] ?>/signin/login">Sign in</a>
-		</div>
-	<?php 
-		}
-	} else {
-	?>
-		<div class="signin">
-			<a href="http://<?=$_SERVER['SERVER_NAME'] ?>/signin/login">Sign in</a>
-		</div>
-	<?php 
-	}
-?>
-	</header>
-<div class="row">
-	<div class="col-md-4">
-		<?php
-		if (isset($this->session)){
-			echo($this->session->flashdata('message'));
-		} 
-		?>
-	</div>
-</div>
->>>>>>> FETCH_HEAD
+
+
