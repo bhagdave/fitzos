846e92120f8fc9f55a03d4d42b5e50183a42401a
Russ Brown <poa02rb@yahoo.co.uk>
2014-03-28 09:00:12 +0000

    Athlete view html changes

--- fuel/application/views/athlete/view.php
+++ fuel/application/views/athlete/view.php
@@ -5,8 +5,8 @@
 		$isMember = False;
 	}
 ?>
-<div class="row">
-	<div class="col-md-2">
+<div class="athlete">
+	<div class="athlete-info">
 		<h2><?=isset($member) ? $member->first_name . ' ' . $member->last_name : '' ; ?></h2>
 		<p>Nickname:<?= isset($athlete->nickname) ? $athlete->nickname :'' ?></p>
 		<p>DOB:<?= isset($athlete->dob) ? $athlete->dob :'' ?></p>
@@ -15,76 +15,64 @@
 		<p>Country:<?= isset($athlete->country) ? $athlete->country :'' ?></p>
 		<p>Location:<?= isset($athlete->location) ? $athlete->location :'' ?></p>
 	</div>
-	<div class="col-md-4">
+	<div class="athlete-img">
 		<?php 
 			if (isset($member) && isset($member->image)){
 				echo("<img src='/$member->image'>");
 			}
 		?>
 	</div>
-	<div class="col-md-4">
-		<h2>Their Sports</h2>
-	<?php
-		if (isset($sports)){
-			foreach($sports as $sport){
-				echo("<h4>$sport->sport</h4>");
-				if (isset($sport->from_date)){
-					$date = new DateTime($sport->from_date);
-					echo("From: ". $date->format('m/d/Y'));
-				}
-				if (isset($sport->to_date)){
-					$date = new DateTime($sport->to_date);
-					echo(" Until " . $date->format('m/d/Y'));
-				}
-				echo("<br>");
-				$existingSports[] = $sport->sport;
-			}
-		}
-	?>
-	</div>
-	<div class="col-md-2">
-		<?php $this->load->view('calendar/bySport'); ?>
-	</div>
 </div>
-<div class="row">
-	<div class="col-md-3">
+<div class="athlete-sports">
+	<h2>Their Sports</h2>
+<?php
+	if (isset($sports)){
+		foreach($sports as $sport){
+			echo("<h4>$sport->sport</h4>");
+			if (isset($sport->from_date)){
+				$date = new DateTime($sport->from_date);
+				echo("From: ". $date->format('m/d/Y'));
+			}
+			if (isset($sport->to_date)){
+				$date = new DateTime($sport->to_date);
+				echo(" Until " . $date->format('m/d/Y'));
+			}
+			echo("<br>");
+			$existingSports[] = $sport->sport;
+		}
+	}
+?>
+</div>
+
+<div class="athlete-calendar">
+	<?php $this->load->view('calendar/bySport'); ?>
+</div>
+
+	<div class="athlete-friends">
 		<h2>Friends</h2>
 		<?php $this->load->view('athlete/friendList'); ?>
 	</div>
-</div>
-<div>
 <?php
 	if ($isMember){
 		echo("<a href='/athlete/profile'>Edit your profile</a><br/>");
 		echo("<a href='#' class='js-emailFriend'>Email Invite To Friend</a>");
-	?>
-		<div class="row">
-			<div class="col-md-6 col-lg-10 col-xs-12">
-					<div class="row">
-						<div class="col-md-3 col-xs-12 col-lg-6">
-							<h2>Notifications</h2>
-						</div>
-						<div class="col-md-3 col-xs-12 col-lg-6">
-							<?php $this->load->view('athlete/notifications'); ?>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-3 col-xs-12 col-lg-6">
-							<h2>Events</h2>
-						</div>
-						<div class="col-md-4 col-xs-12 col-lg-6">
-							<?php $this->load->view('athlete/events'); ?>
-						</div>
-					</div>			
-			</div>
-		</div>
+	?>">
+
+	<div class="athlete-notifications">
+		<h2>Notifications</h2>
+		<?php $this->load->view('athlete/notifications'); ?>
+	</div>
+
+	<div class="athlete-events">
+		<h2>Events</h2>
+		<?php $this->load->view('athlete/events'); ?>
+	</div>		
 
 	<?php
 	} else {
 		echo("<a href='/athlete/beFriend/".$member->id."'>Friend Request</a>");
 	}
 ?>
-</div>
 <?php 
 	$this->load->view('athlete/inviteFriend');
 ?>
