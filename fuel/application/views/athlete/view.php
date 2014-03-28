11afa9df4e316ff67900c85edb1f57d7d1057fa3
Russ Brown <poa02rb@yahoo.co.uk>
2014-03-28 16:56:25 +0000

    athlete profile page
    Styles for athlete profile at athlete/index
    footer fixed so it doesn’t obscure buttons

--- fuel/application/views/athlete/view.php
+++ fuel/application/views/athlete/view.php
@@ -5,67 +5,74 @@
 		$isMember = False;
 	}
 ?>
-<div class="athlete">
-	<div class="athlete-info">
-		<h2><?=isset($member) ? $member->first_name . ' ' . $member->last_name : '' ; ?></h2>
-		<p>Nickname:<?= isset($athlete->nickname) ? $athlete->nickname :'' ?></p>
-		<p>DOB:<?= isset($athlete->dob) ? $athlete->dob :'' ?></p>
-		<p>Gender:<?= isset($athlete->gender) ? $athlete->gender :'' ?></p>
-		<p>Activity:<?= isset($athlete->activity) ? $athlete->activity :'' ?></p>
-		<p>Country:<?= isset($athlete->country) ? $athlete->country :'' ?></p>
-		<p>Location:<?= isset($athlete->location) ? $athlete->location :'' ?></p>
-	</div>
-	<div class="athlete-img">
+<div class="athlete-profile">
+	<div class="athlete-profile__img">
 		<?php 
 			if (isset($member) && isset($member->image)){
 				echo("<img src='/$member->image'>");
 			}
 		?>
 	</div>
-</div>
-<div class="athlete-sports">
-	<h2>Their Sports</h2>
-<?php
-	if (isset($sports)){
-		foreach($sports as $sport){
-			echo("<h4>$sport->sport</h4>");
-			if (isset($sport->from_date)){
-				$date = new DateTime($sport->from_date);
-				echo("From: ". $date->format('m/d/Y'));
+	<div class="athlete-profile__goal">
+		<div class="goal-indicator"></div>
+		<i class="fa fa-star fa-3x"></i>
+	</div>
+	<div class="athlete-profile__calendar">
+		<div class="calendar"></div>
+		<i class="fa fa-calendar fa-3x"></i>
+	</div>
+	<div class="athlete-profile__actions">
+		<i class="fa fa-user fa-3x js-emailFriend"></i>
+		<i class="fa fa-gift fa-3x"></i>
+		<i class="fa fa-inbox fa-3x"></i>
+	</div>
+
+	<?php
+	if ($isMember){
+		// echo("<a href='/athlete/profile'>Edit your profile</a><br/>");
+		// echo("<a href='#' class='js-emailFriend'>Email Invite To Friend</a>");
+	?>
+
+	<div class="athlete-profile__sports">
+		<h4>Sports</h4>
+		<?php
+			if (isset($sports)){
+				foreach($sports as $sport){
+					echo("<h5>$sport->sport</h5>");
+					if (isset($sport->from_date)){
+						$date = new DateTime($sport->from_date);
+						echo("From: ". $date->format('m/d/Y'));
+					}
+					if (isset($sport->to_date)){
+						$date = new DateTime($sport->to_date);
+						echo(" Until " . $date->format('m/d/Y'));
+					}
+					echo("<br>");
+					$existingSports[] = $sport->sport;
+				}
 			}
-			if (isset($sport->to_date)){
-				$date = new DateTime($sport->to_date);
-				echo(" Until " . $date->format('m/d/Y'));
-			}
-			echo("<br>");
-			$existingSports[] = $sport->sport;
-		}
-	}
-?>
+		?>
+	</div>
+
+	<div class="athlete-profile__events">
+		<h4>Events</h4>
+		<?php $this->load->view('athlete/events'); ?>
+	</div>
+
 </div>
 
 <div class="athlete-calendar">
-	<?php $this->load->view('calendar/bySport'); ?>
+	<?php // $this->load->view('calendar/bySport'); ?>
 </div>
 
 	<div class="athlete-friends">
-		<h2>Friends</h2>
-		<?php $this->load->view('athlete/friendList'); ?>
+		<!-- 		<h2>Friends</h2> -->
+		<?php // $this->load->view('athlete/friendList'); ?>
 	</div>
-<?php
-	if ($isMember){
-		echo("<a href='/athlete/profile'>Edit your profile</a><br/>");
-		echo("<a href='#' class='js-emailFriend'>Email Invite To Friend</a>");
-	?>">
 
 	<div class="athlete-notifications">
-		<h2>Notifications</h2>
-		<?php $this->load->view('athlete/notifications'); ?>
-	</div>
-
-	<div class="athlete-events">
-		<h2>Events</h2>
-		<?php $this->load->view('athlete/events'); ?>
+		<!-- 		<h2>Notifications</h2> -->
+		<?php // $this->load->view('athlete/notifications'); ?>
 	</div>		
 
 	<?php
