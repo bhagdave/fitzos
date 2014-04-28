dc9f928d6818b07ee81b4baf8347152eb6b844c0
bhagdave <dave_gill@blueyonder.co.uk>
2014-04-28 15:18:10 +0100

    Fixed some models not working for CMS modules

--- fuel/application/models/exercises_model.php
+++ fuel/application/models/exercises_model.php
@@ -1,6 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
  
-class Exercises_model extends Base_module_model {
+class Exercises_model extends Fitzos_model {
  
     function __construct()
     {
--- fuel/application/models/forums_model.php
+++ fuel/application/models/forums_model.php
@@ -1,6 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
  
-class Forums_model extends Base_module_model {
+class Forums_model extends Fitzos_model {
  
     function __construct()
     {
