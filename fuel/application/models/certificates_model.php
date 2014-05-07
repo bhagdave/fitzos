b957f65d90b3c17359be9c06472f2e7b8b79c8b8
bhagdave <dave_gill@blueyonder.co.uk>
2014-05-07 11:00:12 +0100

    New models for CMS

---
+++ fuel/application/models/certificates_model.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Certificates_model extends Fitzos_model {
+
+	function __construct()
+	{
+		parent::__construct('trainer_certificates');
+	}
+}
