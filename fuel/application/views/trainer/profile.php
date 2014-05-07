0066a386532dce80174336d23297dba66fc806ee
bhagdave <dave_gill@blueyonder.co.uk>
2014-05-07 12:35:00 +0100

    Created profile page for trainer entry

---
+++ fuel/application/views/trainer/profile.php
@@ -0,0 +1,33 @@
+<div class="row">
+	<div class="col-md-6">
+		<form class="form-horizontal" action="/trainers/profile" method="post" enctype="multipart/form-data">
+			<fieldset id="inputs">
+				<?php $this->load->view('_blocks/all-members');?>
+				<div class="control-group">
+					<label class="control-label">Nickname</label>
+					<div class="controls">
+						<input id="nickname" class="form-control" data-bind="value: nickname" name="nickname"  type="text" placeholder="Nickname">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Biography</label>
+					<div class="controls">
+						<textarea id="bio" class="form-control" name="bio"></textarea>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label">Profile image</label>
+					<?php 
+						if (isset($member->image)){
+							echo("<img src='/$member->image'>");
+						} 
+					?>
+					<div class="controls">	
+						<input type="file" name="file" id="file" value="<?=isset($member->image) ? $member->image : ''  ?>">
+					</div>
+				</div>
+				<button class="btn btn-success" >Submit</button>	
+			</fieldset>
+		</form>
+	</div>
+</div>
