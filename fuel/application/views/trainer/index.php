5f21f2ebfd273e65a76ba3b80a8d844ff0ecc986
bhagdave <dave_gill@blueyonder.co.uk>
2014-05-07 11:18:30 +0100

    First attempt at TRainer index page

--- fuel/application/views/trainer/index.php
+++ fuel/application/views/trainer/index.php
@@ -1,1 +1,23 @@
 <?php
+	if (isset($id) && $id == $member->id){
+		$isMember = True;
+	} else {
+		$isMember = False;
+	}
+?>
+<div class="row">
+	<div class="col-md-4 athlete-profile__img">
+		<?php 
+			if (isset($member) && isset($member->image)){
+				echo("<img src='/$member->image'>");
+			}
+		?>
+
+	<?php
+	if (isset($isMember) && $isMember){
+		 echo("<a href='/trainer/profile'>Edit your profile</a><br/>");
+		 echo("<a href='#' class='js-emailFriend'>Email Invite To Friend</a>");
+	}
+	?>
+	</div>
+</div>
