ee82e968e4ccba84a78b5f6ae9564f547d15e40d
bhagdave <dave_gill@blueyonder.co.uk>
2014-05-08 11:24:24 +0100

    New number field for qualification

--- Scripts/trainer_qualifications.sql
+++ Scripts/trainer_qualifications.sql
@@ -1,1 +1,1 @@
-CREATE  TABLE `fitzos`.`trainer_qualifications` (  `trainer_id` INT NOT NULL ,  `certificate_id` INT NOT NULL ,  `date` VARCHAR(45) NULL ,  `validated` ENUM('yes','no') NULL ,  PRIMARY KEY (`trainer_id`, `certificate_id`) );
+CREATE  TABLE `fitzos`.`trainer_qualifications` (  `trainer_id` INT NOT NULL ,  `certificate_id` INT NOT NULL ,  `date` VARCHAR(45) NULL ,  `validated` ENUM('yes','no') NULL , `number` VARCHAR(145) NULL , PRIMARY KEY (`trainer_id`, `certificate_id`) );
