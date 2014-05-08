ec318b244050056d98ed7a81c3ff1c0943794004
bhagdave <dave_gill@blueyonder.co.uk>
2014-05-08 11:16:17 +0100

    New table for trainer quals

---
+++ Scripts/trainer_qualifications.sql
@@ -0,0 +1,1 @@
+CREATE  TABLE `fitzos`.`trainer_qualifications` (  `trainer_id` INT NOT NULL ,  `certificate_id` INT NOT NULL ,  `date` VARCHAR(45) NULL ,  `validated` ENUM('yes','no') NULL ,  PRIMARY KEY (`trainer_id`, `certificate_id`) );
