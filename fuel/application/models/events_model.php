b92259b79614c6873ad7a83d54d701090f514e0f
bhagdave <dave_gill@blueyonder.co.uk>
2014-06-03 09:28:47 +0100

    Removed echo from getPublicEvents

--- fuel/application/models/events_model.php
+++ fuel/application/models/events_model.php
@@ -52,7 +52,7 @@ class Events_model extends Fitzos_model {
     	$this->db->order_by('event.date');
     	$this->db->limit(10);
     	$result = $this->db->get('event');
-    	echo($this->db->last_query());
+//    	echo($this->db->last_query());
     	return $result->result();
     }
     function getPublicEventsForMonthBySport(){
