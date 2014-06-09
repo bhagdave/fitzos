12f6868e0094e37b249e27d608285189c7fe0825
bhagdave <dave_gill@blueyonder.co.uk>
2014-06-09 11:49:00 +0100

    Changed session creation

--- fuel/application/models/api_model.php
+++ fuel/application/models/api_model.php
@@ -7,7 +7,7 @@ class Api_model extends Base_module_model {
     {
         parent::__construct('api_access');
     }
-    function openSession($name,$key){
+    function openSession($name,$key,$ipAddress = ''){
     	$this->logEvent('OpenSession',"name=$name&key=$key");
     	$this->db->select("name,session_key");
     	$this->db->where('name',$name);
@@ -15,12 +15,10 @@ class Api_model extends Base_module_model {
     	$result = $this->db->get('api_access');
     	$data = $result->result();
     	if (isset($data[0])){
-    		// lets create a session key and wack it in..
+    		// lets create a session key and wack it in to the session table....
     		$session_key = md5("name=$name&key=$key" . date("H:i:s"));
-    		// update the session key and timestamp
-    		$this->db->where('name',$name);
-    		$this->db->where('key',$key);
-    		$this->db->update('api_access',array('session_key'=>$session_key,'timestamp'=>date("Y-m-d H:i:s")));
+    		// create the session key and timestamp
+    		$this->db->insert('session',array('session_name'=>$name,'session_key'=>$key,'ip_address'=>$ipAddress,'session_key'=>$session_key,'timestamp'=>date("Y-m-d H:i:s")));
     		return $session_key;
     	} else {
     		$this->logEvent('OpenSession Failed',"name=$name&key=$key");
