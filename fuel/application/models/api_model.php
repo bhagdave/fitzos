579698051cfa48a1029300179f7afd5adc290bad
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-20 14:47:10 +0100

    Put the signature into lower case for testing

--- fuel/application/models/api_model.php
+++ fuel/application/models/api_model.php
@@ -46,7 +46,7 @@ class Api_model extends Base_module_model {
     	if (isset($data[0])){
     		$test = md5($data[0]->session_name . $data[0]->key. $method);
     		$this->logEvent('isValidSessionKey',"Session found test=$test");    		
-    		if ($test == $signature){
+    		if (strtolower($test) == strtolower($signature)){
     			return true;
     		} else {
     			$this->logEvent('isValidSessionKey',"test!=signature");    		
