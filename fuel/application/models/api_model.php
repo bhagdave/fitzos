ef031e2b4d1ff76be9538362fee504bec0061fd4
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-20 14:44:52 +0100

    Added log events to api to see what is happening.

--- fuel/application/models/api_model.php
+++ fuel/application/models/api_model.php
@@ -36,21 +36,24 @@ class Api_model extends Base_module_model {
 		$this->db->insert('api_log',$insert);
 	}
 	function isValidSessionKey($method,$key, $signature){
-		echo("Method=$method Key=$key Signature=$signature\n");
+		$this->logEvent('isValidSessionKey',"Method=$method Key=$key Signature=$signature");
     	$this->db->where('session.session_key',strtolower($key));
 //    	$this->db->where('session.timestamp >','now() + interval - 1 hour');
     	$this->db->join('api_access','api_access.name = session.session_name');
     	$result = $this->db->get('session');
-    	echo($this->db->last_query());
+    	$this->logEvent('isValidSessionKey',$this->db->last_query());
     	$data = $result->result();
     	if (isset($data[0])){
     		$test = md5($data[0]->session_name . $data[0]->key. $method);
+    		$this->logEvent('isValidSessionKey',"Session found test=$test");    		
     		if ($test == $signature){
     			return true;
     		} else {
+    			$this->logEvent('isValidSessionKey',"test!=signature");    		
     			return false;
     		}
     	} else {
+    		$this->logEvent('isValidSessionKey',"No session found!");    		
     		return false;
     	}
 	}
