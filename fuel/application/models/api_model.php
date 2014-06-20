92453f8bb866c1219ac93d942cced9b8c8ed8f7c
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-20 14:33:26 +0100

    Convert key to lower case for checking on DB

--- fuel/application/models/api_model.php
+++ fuel/application/models/api_model.php
@@ -37,7 +37,7 @@ class Api_model extends Base_module_model {
 	}
 	function isValidSessionKey($method,$key, $signature){
 //		echo("Method=$method Key=$key Signature=$signature\n");
-    	$this->db->where('session.session_key',$key);
+    	$this->db->where('session.session_key',strtolower($key));
 //    	$this->db->where('session.timestamp >','now() + interval - 1 hour');
     	$this->db->join('api_access','api_access.name = session.session_name');
     	$result = $this->db->get('session');
