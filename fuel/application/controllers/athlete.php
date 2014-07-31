2a5c6687b2f84bb02f8622a7960ff321c4e49a0d
Dave Gill <dave_gill@blueyonder.co.uk>
2014-07-31 21:51:29 +0100

    Removed some logging

--- fuel/application/controllers/athlete.php
+++ fuel/application/controllers/athlete.php
@@ -174,7 +174,6 @@ class Athlete extends CI_Controller{
 	function saveProfileImage($id){
 		$request = $_REQUEST;
 		$this->load->model('api_model','x');
-		$this->x->logEvent('saveProfileImage','ENTRY');
 		$this->load->helper('inflector');
 		if (isset($_FILES['file']['name']) && !empty($_FILES['file']['name'])){
 			if ($_FILES["file"]["error"] > 0){
