1c78c468441a05b926c2ffcdf619c5f09d343f8a
Dave Gill <dave_gill@blueyonder.co.uk>
2014-08-11 20:58:24 +0100

    Changes to logging

--- fuel/application/controllers/api.php
+++ fuel/application/controllers/api.php
@@ -37,6 +37,7 @@ class Api extends CI_Controller{
 	function r($model,$function){
 		$data = $_REQUEST;
 		$modelName = $model . '_model';
+		$this->api->logEvent($model . '->' . $function,print_r($data,true));
 		$err = $this->load->model($modelName,$model);
 		if (isset($err)){
 			if (isset($data['member_id']) && !is_numeric($data['member_id'])){
@@ -59,7 +60,7 @@ class Api extends CI_Controller{
 	}
 	
 	function index($model,$function){
-		$this->api->logEvent($model . '->' . $function,print_r($_REQUEST,true));
+//		$this->api->logEvent($model . '->' . $function,print_r($_REQUEST,true));
 //		if ($this->_checkSessionKey($function)){
 			$data = $_REQUEST;
 			$modelName = $model . '_model';
@@ -75,7 +76,7 @@ class Api extends CI_Controller{
 						// convert salt to memberid
 						$data['member_id'] = $this->_convertMemberSaltToId($data['member_id']);
 					}
-					$this->api->logEvent($model . '->' . $function . ' PRECALL',print_r($data,true));
+//					$this->api->logEvent($model . '->' . $function . ' PRECALL',print_r($data,true));
 //					$result = $this->doTheMethodCall($model, $function, $data);
  					$result = $this->$model->$function($data['id']);
 				} else {
