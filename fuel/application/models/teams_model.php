86e567ca2e284ba7156d6bcd3b236b8df91461a5
Dave Gill <dave_gill@blueyonder.co.uk>
2014-08-31 20:12:19 +0100

    New method added to send multiple invites

--- fuel/application/models/teams_model.php
+++ fuel/application/models/teams_model.php
@@ -329,6 +329,12 @@ class Teams_model extends Fitzos_model {
 		$done = $this->db->affected_rows();
 		return $done;
 	}
+	function sendInvites($members,$user,$team){
+		$members = json_decode($members);
+		foreach($members as $member){
+			$this->sendInvite($member,$user,$team);
+		}
+	}
 	function sendInvite($memberId,$user,$teamId){
 		$this->load->model('notifications_model');
 		$team = $this->getTeam($teamId);
