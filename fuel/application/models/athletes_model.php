72f923d73a8908e2c11e2f5de07dee6d1d35e96d
Dave Gill <dave_gill@blueyonder.co.uk>
2014-10-02 15:43:40 +0100

    Just get date for sports stats
    Strip time component off

--- fuel/application/models/athletes_model.php
+++ fuel/application/models/athletes_model.php
@@ -59,7 +59,7 @@ class Athletes_model extends Fitzos_model {
 		}
 	}
 	function getStatsForAthleteSport($id,$sport){
-		$this->db->select('statistic_name,statistic_value,date,formula,comment');
+		$this->db->select('statistic_name,statistic_value,date(date),formula,comment');
 		$this->db->where('sport_id',$sport);
 		$this->db->where('source_table','member');
 		$this->db->where('source_id',$id);
