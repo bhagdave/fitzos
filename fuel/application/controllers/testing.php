4e8b208181f9b2995a02a8da34cb936c02717b6c
Dave Gill <dave_gill@blueyonder.co.uk>
2015-03-04 22:32:06 +0000

    Create notification when jointing team
    With unit test

--- fuel/application/controllers/testing.php
+++ fuel/application/controllers/testing.php
@@ -4,4 +4,8 @@ class Testing extends CI_Controller{
 		$this->load->model('events_model','events');
 		$this->events->acceptInvite(1,27);
 	}
+	public function testJoinTeam(){
+		$this->load->model('teams_model','teams');
+		$this->teams->setMemberRequest(6,1);
+	}
 }
